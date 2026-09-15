348655d084dbdf1bca7aa73d7f988207e661e458
Neytrino2134 <neytrino2134@gmail.com>
2026-09-15 14:26:02 +0500

    Bump add-on version to 0.3.1

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "AI Retopo Toolkit",
     "author": "Meowmaster",
-    "version": (0, 3, 0),
+    "version": (0, 3, 1),
     "blender": (5, 0, 0),
     "location": "View3D > Sidebar > AI Retopo",
     "description": "Semi-automatic retopology toolkit for generated AI meshes",
